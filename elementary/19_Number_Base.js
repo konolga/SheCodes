40f8327410a458076ef7d92f454864ebf8dd24bc
Olga Kon <olgasi@payoneer.com>
2019-04-15 13:42:02 +0300

    17-19

---
+++ elementary/19_Number_Base.js
@@ -0,0 +1,25 @@
+/* You are given a positive number as a string along with the radix for it. Your function should convert it into decimal form. The radix is less than 37 and greater than 1. The task uses digits and the letters A-Z for the strings.
+
+Watch out for cases when the number cannot be converted. For example: "1A" cannot be converted with radix 9. For these cases your function should return -1. */
+
+"use strict";
+
+function numberRadix(str_number, radix){
+    const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.substring(0, radix);
+    return str_number.split('').every(e=>digits.indexOf(e)>-1) ? Number.parseInt(str_number, radix) : -1
+
+     
+}
+
+
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+    assert.equal(numberRadix("AF", 16), 175, "Hex");
+    assert.equal(numberRadix("101", 2), 5, "Bin");
+    assert.equal(numberRadix("101", 5), 26, "5 base");
+    assert.equal(numberRadix("Z", 36), 35, "Z base");
+    assert.equal(numberRadix("AB", 10), -1, "B > A > 10");
+    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+}
