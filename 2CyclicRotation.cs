dcf42efc0cd9f6c2889694ba4063f861bffaf91c
Olga Kon <ms.estete@gmail.com>
2019-06-06 16:20:26 +0300

    .

--- 2CyclicRotation.cs
+++ 2CyclicRotation.cs
@@ -6,7 +6,7 @@ namespace Codility
 {
     class _2CyclicRotation
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             int[] Arr = new int[] { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
             int[] res = CyclicRotation(Arr, 7);
---
+++ 4PermCheck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+    class _4PermCheck
+    {
+
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[] { 4, 1, 3, 2 };
+            Console.WriteLine(Solution(Arr));
+            Console.ReadKey();
+        }
+
+
+        public static int Solution(int[] A)
+        {
+
+            A = A.OrderByDescending(el => el).ToArray();
+            if (A.Length == 0 || A[0] != A.Length || A[A.Length - 1] != 1 )
+            {
+                return 0;
+            }
+
+            else
+            {
+                for (int i = 0; i < A.Length - 1; i++)
+                {
+                    if (A[i] - A[i + 1] != 1) //compare two numbers
+                    {
+                        return 0;
+                    }
+
+                }
+                return 1;
+            }
+        }
+           
+    }
+}
